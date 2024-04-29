6baff4c555bbc2bfdf2d2f17c90c568e89a5f975
NezhelskiyViktor <nezhelskiy@yandex.ru>
2024-04-29 19:59:20 +0300

    8. test class Game

--- game.py
+++ game.py
@@ -9,3 +9,17 @@ class Game:
         # (кто атаковал и сколько здоровья осталось у противника)
         # и объявляем победителя.
         print('Игра "Битва героев"')
+        while self.player.is_alive() and self.computer.is_alive():
+            print(f'Ходит {self.player.name}')
+            self.player.attack(self.computer)
+            print(f'Здоровье {self.computer.name}: {self.computer.health}')
+            if not self.computer.is_alive():
+                print(f'Победил {self.player.name}')
+                break
+            print(f'Ходит {self.computer.name}')
+            self.computer.attack(self.player)
+            print(f'Здоровье {self.player.name}: {self.player.health}')
+            if not self.player.is_alive():
+                print(f'Победил {self.computer.name}')
+                break
+
